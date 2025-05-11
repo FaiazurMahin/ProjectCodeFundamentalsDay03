8304b9af086c88d883d7f1bc97d6e58e65deb08b
Faiazur Rahman <faiazurrahman748@gmail.com>
2025-05-12 00:51:56 +0600

    test

---
+++ practice.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+console.log("I'm new to JavaScript"); 
+
+
+//Normal Function
+function greet(){      
+    console.log("Yokosoo")
+}
+
+//Function with Parameter
+function greetagain(name){
+    console.log("Yokosoo," + name+ "!")
+}
+
+//Function with Return
+function add(a,b){
+    return a+b;
+}
+let result = add(a,b);
+console.log(result); 
+
+// 1️⃣ Create a function called subtractNumbers that:
+// Takes two numbers as input.
+// Subtracts the second from the first.
+// Logs the result.
+
+function subtractNumbers(a,c){
+    return a-c;
+}
+let deduction = subtractNumbers(10,5);
+console.log(deduction);
+
